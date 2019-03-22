a8e111d2b1bf1dcb76962f48179fe7984e532a1f
alexisdavid <alexisduarte1512@gmail.com>
2019-03-22 10:57:07 -0500

    terminada primer parte de datepicker

--- src/app/pages/date-time/date-time.page.ts
+++ src/app/pages/date-time/date-time.page.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DateTimePage implements OnInit {
 
+
+  dob: Date = new Date();
   constructor() { }
 
   ngOnInit() {
   }
+  cambioFecha(event) {
+    console.log('ionChange', event);
+    console.log('Date prueba', new Date(event.detail.value));
 
+  }
 }
