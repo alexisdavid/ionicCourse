7220f2ce88654907b6fe93deb1c3128368ea8e2c
alexisdavid <alexisduarte1512@gmail.com>
2019-04-10 12:37:58 -0500

    terminado toast

--- src/app/pages/lista/lista.page.ts
+++ src/app/pages/lista/lista.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { DataService } from 'src/app/services/data.service';
 import { Observable } from 'rxjs';
-import { IonList } from '@ionic/angular';
+import { IonList, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-lista',
@@ -12,7 +12,8 @@ export class ListaPage implements OnInit {
   @ViewChild('lista') lista: IonList
 
   usuarios: Observable<any>;
-  constructor(private dataService: DataService) { }
+  constructor(private dataService: DataService,
+    private toastController: ToastController) { }
 
   ngOnInit() {
 
@@ -21,20 +22,43 @@ export class ListaPage implements OnInit {
 
   }
 
+
+  async presentToast(message: string) {
+    const toast = await this.toastController.create({
+      // message: 'Your settings have been saved.'
+      message,
+      duration: 2000
+    });
+    toast.present();
+  }
+
+  async presentToastWithOptions() {
+    const toast = await this.toastController.create({
+      message: 'Click to Close',
+      showCloseButton: true,
+      position: 'bottom',
+      closeButtonText: 'Done'
+    });
+    toast.present();
+  }
+
   favorite(user) {
-    console.log('favorite', user);
+    // console.log('favorite', user);
+    this.presentToast('favorite');
     this.lista.closeSlidingItems();
 
 
   }
   share(user) {
-    console.log('share', user);
+    // console.log('share', user);
+    this.presentToast('share');
     this.lista.closeSlidingItems();
 
 
   }
   unread(user) {
-    console.log('unread', user);
+    // console.log('unread', user);
+    this.presentToastWithOptions();
     this.lista.closeSlidingItems();
 
   }
